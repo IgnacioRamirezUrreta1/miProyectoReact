29b82f6853ccf3f5c76e77e1599d38f33db4f470
IgnacioRamirezUrreta1 <127261317+IgnacioRamirezUrreta1@users.noreply.github.com>
2025-07-24 00:24:29 -0300

    Primer commit con monorepo y proyecto Vite React

---
+++ proyects/hola-react/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+
+  <React.Fragment>
+    <button>Hola boton</button>
+    <button>Hola boton</button>
+  </React.Fragment>
+
+ )
